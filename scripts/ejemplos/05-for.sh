8b93dff0f0067ede0e4f5b231c1ebc6cb37c77ac
jcmenaartero <jcmenaartero@gmail.com>
2021-12-14 00:17:12 +0100

    2021/12/14

---
+++ scripts/ejemplos/05-for.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# Autor: juan
+# Fecha: 2021/12/13 20:30
+# Descripcion: Ejemplo del bucle for
+
+clear
+lista='Mario Juan Pepe'
+for i in $lista
+do
+    echo "Hola me llamo $i"
+done
+
+exit 0
+
